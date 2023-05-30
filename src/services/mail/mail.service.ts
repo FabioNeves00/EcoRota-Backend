82f36873286f1abbc498423931c2f4c1561b2486
Fabio Neves <fabinhoneves09@gmail.com>
2023-05-30 11:51:12 -0300

    using path to concat

--- src/services/mail/mail.service.ts
+++ src/services/mail/mail.service.ts
@@ -1,6 +1,7 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { TicketDto } from './dto/ticket.dto';
+import path from 'path';
 
 
 @Injectable()
@@ -8,6 +9,7 @@ export class MailService {
   constructor(private mailerService: MailerService) {}
 
   async sendTicket(ticket: any, ticketDto: TicketDto) {
+    
     await this.mailerService.sendMail({
       subject: "Ingresso para a experiência EcoRota",
       to: ticketDto.email,
@@ -23,11 +25,11 @@ export class MailService {
         cid: 'ticket'
       },{
         filename: 'banner.jpeg',
-        path: `${process.cwd()}/src/assets/banner_obrigado.jpeg`,
+        path: path.join(process.cwd(), "src", "assets", "banner_obrigado.jpeg"),
         cid: 'banner'
       },{
         filename: 'logo.jpeg',
-        path: `${process.cwd()}/src/assets/logomarca_ecorota.jpeg`,
+        path: path.join(process.cwd(), "src", "assets", "logomarca_ecorota.jpeg"),
         cid: 'logo'
       },],
       attachDataUrls: true,
